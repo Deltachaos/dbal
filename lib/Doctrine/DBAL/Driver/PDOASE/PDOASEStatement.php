f222ec7b6150f253c5ffd0a49f367c63a74ad3ff
Maximilian Ruta <mr@xtain.net>
2017-09-15 13:39:13 +0200

    cache results in case of parallel query execution as pdo_dblib leaks support for this

--- lib/Doctrine/DBAL/Driver/PDOASE/PDOASEStatement.php
+++ lib/Doctrine/DBAL/Driver/PDOASE/PDOASEStatement.php
@@ -30,6 +30,16 @@ use Doctrine\DBAL\Driver\PDOStatement;
 class PDOASEStatement extends PDOStatement
 {
     /**
+     * @var self
+     */
+    protected static $lastActive;
+
+    /**
+     * @var array
+     */
+    protected $resultCache;
+
+    /**
      * @param mixed $value
      * @param int   $type
      *
@@ -77,7 +87,7 @@ class PDOASEStatement extends PDOStatement
     /**
      * {@inheritdoc}
      */
-    public function bindValue($param, $value, $type = \PDO::PARAM_STR)
+    public function bindValue($param, $value, $type = null)
     {
         if ($type === null) {
             $type = $this->guessType($value);
@@ -93,7 +103,7 @@ class PDOASEStatement extends PDOStatement
     /**
      * {@inheritdoc}
      */
-    public function bindParam($column, &$variable, $type = \PDO::PARAM_STR, $length = null, $driverOptions = null)
+    public function bindParam($column, &$variable, $type = null, $length = null, $driverOptions = null)
     {
         if ($type === null) {
             $type = $this->guessType($variable);
@@ -101,6 +111,67 @@ class PDOASEStatement extends PDOStatement
 
         $variable = $this->fixType($variable, $type);
         return parent::bindParam($column, $variable, $type, $length, $driverOptions);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function fetchColumn($columnIndex = 0)
+    {
+        if (isset($this->resultCache)) {
+            if (count($this->resultCache) == 0) {
+                return false;
+            }
+
+            $item = array_values(array_shift($this->resultCache));
+
+            return $item[$columnIndex];
+        } else {
+            return parent::fetchColumn($columnIndex);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function fetch($fetchMode = null, $cursorOrientation = null, $cursorOffset = null)
+    {
+        if (isset($this->resultCache)) {
+            if ($fetchMode != \PDO::FETCH_ASSOC && $cursorOrientation !== null || $cursorOffset !== null) {
+                throw new \RuntimeException('result caching is only implemented for PDO::FETCH_ASSOC');
+            }
+
+            if (count($this->resultCache) == 0) {
+                return false;
+            }
+
+            return array_shift($this->resultCache);
+        } else {
+            return parent::fetch($fetchMode, $cursorOrientation, $cursorOffset);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function fetchAll($fetchMode = null, $fetchArgument = null, $ctorArgs = null)
+    {
+        if (isset($this->resultCache)) {
+            if ($fetchMode != \PDO::FETCH_ASSOC && $fetchArgument !== null || $ctorArgs !== null) {
+                throw new \RuntimeException('result caching is only implemented for PDO::FETCH_ASSOC');
+            }
+
+            $data = $this->resultCache;
+            $this->resultCache = array();
+            return $data;
+        } else {
+            return parent::fetchAll($fetchMode, $fetchArgument, $ctorArgs);
+        }
+    }
+
+    protected function cacheResults()
+    {
+        $this->resultCache = parent::fetchAll(\PDO::FETCH_ASSOC);
     }
 
     /**
@@ -118,7 +189,13 @@ class PDOASEStatement extends PDOStatement
             }
         }
 
+        if (self::$lastActive instanceof self) {
+            self::$lastActive->cacheResults();
+        }
+
         $result = parent::execute();
+
+        self::$lastActive = $this;
 
         // forward to the result set which is not empty
         while ($this->columnCount() == 0) {
